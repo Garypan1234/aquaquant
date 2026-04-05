6e4269840dc33d7901ece64c60a8adcab4f2bb8c
AquaQuant <96157660+Garypan1234@users.noreply.github.com>
2026-04-05 15:15:52 -0400

    Update Stock_bot.py

--- Stock_bot.py
+++ Stock_bot.py
@@ -65,3 +65,18 @@ for stock in stocks:
         print("📈 RSI > 70 -> 超买")
     else:
         print("RSI 正常")
+import requests
+import os
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    requests.post(url, data=payload)
+
+send_telegram("🔥 GitHub测试成功：Telegram已连接！")
